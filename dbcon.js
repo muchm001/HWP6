a2ba9c452bc17dac43542a5f0a8eabe4672f8708
Michael <muchm001@gmail.com>
2021-03-13 00:52:26 -0600

    ok

--- dbcon.js
+++ dbcon.js
@@ -3,7 +3,7 @@ var pool = mysql.createPool({
   connectionLimit : 10,
   host : 'classmysql.engr.oregonstate.edu',
   user : 'cs290_uchmanom',
-  password : '2316',
+  password : '8326',
   database : 'cs290_uchmanom',
   dateStrings : 'date'
 });
